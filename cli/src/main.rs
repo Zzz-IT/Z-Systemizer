5129ba8118d8cf92658f1929a45b43d81f13094e
Zzz <2384166487@qq.com>
2026-06-28 17:01:11 +0800

    refactor: 恢复 main.rs 中更优雅的 KEEPALIVE_TAGS 循环生成方式，并修正 CI 的 grep 匹配规则

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -877,14 +877,9 @@ fn update_aosp_keepalive_sysconfig(state: &StateFile) -> Result<(), String> {
     for pkg in packages {
         let pkg = xml_escape(&pkg);
 
-        xml.push_str(&format!(
-            "    <allow-in-power-save package=\"{}\" />\n",
-            pkg
-        ));
-        xml.push_str(&format!(
-            "    <allow-in-power-save-except-idle package=\"{}\" />\n",
-            pkg
-        ));
+        for tag in KEEPALIVE_TAGS {
+            xml.push_str(&format!("    <{} package=\"{}\" />\n", tag, pkg));
+        }
     }
 
     xml.push_str("</config>\n");
